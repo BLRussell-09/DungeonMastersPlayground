929b84c88b5e2a71d206cceaa1dd7768c7c1d0cd
Barry Russell <blrussell09@gmail.com>
2019-01-05 15:10:26 -0600

    Add project files.

---
+++ DungeonMastersPlayground/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DungeonMastersPlayground.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
