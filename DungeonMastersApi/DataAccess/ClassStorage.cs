cd4cb99858f9bf7551dd4558e9815dc9e8465616
Barry Russell <blrussell09@gmail.com>
2019-02-05 20:04:51 -0600

    users can now get stock classes from the database

--- DungeonMastersApi/DataAccess/ClassStorage.cs
+++ DungeonMastersApi/DataAccess/ClassStorage.cs
@@ -25,6 +25,21 @@ namespace DungeonMastersApi.DataAccess
         connection.Open();
         var result = connection.Query<Class>(@"Select * from Class as c Where c.[index] = @index", new { index = index });
 
+        var classId = result.ElementAt(0)._id;
+        var proficiencies = connection.Query<Proficiency>(@"Select * from Proficiencies as p Where p.class_id = @classId", new { classId = classId});
+        var savingThrows = connection.Query<Saving_Throws>(@"Select * from Saving_Throws as st Where st.class_id = @classId", new { classId = classId});
+        var proChoices = connection.Query<ProficiencyChoice>(@"Select * from Proficiency_Choices as pc Where pc.class_id = @classId", new { classId = classId });
+        var pros = connection.Query<From>(@"Select * from FromP as f Where f.class_id = @classId", new { classId = classId });
+
+        result.ElementAt(0).proficiencies = proficiencies.ToList();
+        result.ElementAt(0).saving_throws = savingThrows.ToList();
+        result.ElementAt(0).proficiency_choices = proChoices.ToList();
+
+        foreach (var choice in result.ElementAt(0).proficiency_choices)
+        {
+          choice.from = pros.ToList();
+        }
+
         return result;
       }
     }
